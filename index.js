79da53f93f6b8456c07266b199ff0f1cdcc9f974
Chely Ho <adept_inept@yahoo.com>
2017-11-06 16:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function changeLeastFavoriteCustomer(){
 
 function attemptTwoFavoriteCustomers(){
   let favoriteCustomer = 'bob';
-  favoritecustomer = 'bob, really';
+  let favoritecustomer = 'bob, really';
 }
