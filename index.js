8d37d404058d40890fb005d81050e112ad2b95ab
Chely Ho <adept_inept@yahoo.com>
2017-11-06 16:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-  customerName = customerName.toUpperCase();
-  return customerName;
+  str = customerName.toUpperCase();
+  return str;
 }
